1c056c9e87530de154f55a8509ceb36a54fcc117
fr3d1s4nm4r <alsamapa@gmail.com>
2021-09-22 16:12:13 +0200

    Updated login html and register html to request php in post and register system

---
+++ register.php
@@ -0,0 +1,28 @@
+<?php 
+include("db-connection.php");
+
+$username = $_POST['username'];
+$real_name = $_POST['real_name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+print "$username\n\r";
+print "$real_name\n\r";
+print "$email\n\r";
+print "$password\n\r";
+
+//hash the password
+$hashed = hash("sha512", $password);
+
+$sql = "INSERT INTO users (id, username, realname, email, password) VALUES (2, '$username', '$real_name', '$email', '$hashed')";
+if(mysqli_query($conn, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+
+?>
