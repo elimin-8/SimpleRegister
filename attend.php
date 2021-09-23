82f0d178db4430d16ef81ca32a38ff3c132ce997
TeslaWasRobbed <nathantgardner222@hotmail.com>
2021-09-23 14:01:02 +0100

    user attend php

--- attend.php
+++ attend.php
@@ -1,5 +1,12 @@
 <?php
-     if($_SESSION['login_user']==true){ 
-       echo "<button type ='button'>Mark Attendance</button>"
+    session_start();  
+
+    if($_SESSION['login_user']==true){ 
+        echo "<button type ='button'>Mark Attendance</button>";
+    }
+
+    if ($_SERVER["REQUEST_METHOD"]=="POST") {
+        $username = $_SESSION["login_user"];
+        echo $username;
     }
 ?>
